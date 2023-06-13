0e517312e97ef7ce282108469d7342bab9a9ab2f
joelhulen <joelhulen@gmail.com>
2023-06-13 00:04:24 -0400

    Add claim procedure and member list functions

---
+++ src/CoreClaims.FunctionApp/HttpTriggers/ListMembers.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using CoreClaims.Infrastructure.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CoreClaims.FunctionApp.HttpTriggers
+{
+    public class ListMembers
+    {
+        private readonly IMemberRepository _repository;
+
+        public ListMembers(IMemberRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [FunctionName("ListMembers")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "member")] HttpRequest req,
+            ILogger log)
+        {
+            using (log.BeginScope("HttpTrigger: ListPayers"))
+            {
+                var (offset, limit) = req.GetPagingQuery();
+
+                var result = await _repository.ListMembers(offset, limit);
+                return new OkObjectResult(result);
+            }
+        }
+    }
+}
