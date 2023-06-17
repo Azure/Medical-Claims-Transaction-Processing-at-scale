d7d4f09b870d6bcc51dc3fe1f0a4fafc31ddd94d
Carlos Retes <carlos.retes@simpat.tech>
2023-06-16 18:28:26 -0600

    Member Details
    Add link button for display a member details from table results

--- ui/medical-claims-ui/src/app/hooks/TransactionsStatement.js
+++ ui/medical-claims-ui/src/app/hooks/TransactionsStatement.js
@@ -3,25 +3,29 @@
 import useSWR from "swr";
 import axios from "axios";
 
-//const fetcher = (url) => axios.get(url, { headers: { 'x-function-key': process.env.X_FUNCTION_KEY } }).then((res) => res.data);
-const fetcher = (url) => axios.get(url, { headers: {'x-functions-key': '-3VTIyOmv6Fj3cMi_kaVcwPHEr2nN8qM7ks4zNzdfzZ0AzFu8YepTw==' }}).then((res) => res.data);
+const 
+	API_URL = 'https://fa-coreclaims-ios38n2l.azurewebsites.net/api',//process.env.API_URL,
+	X_FUNCTION_KEY = '-3VTIyOmv6Fj3cMi_kaVcwPHEr2nN8qM7ks4zNzdfzZ0AzFu8YepTw=='//process.env.X_FUNCTION_KEY
 
-const getMembersList = (memberId, pageSize = 10) => {
-	let apiUrl = process.env.API_URL;
-	let xFunctionKey = process.env.X_FUNCTION_KEY;
-	
-	// return useSWR(
-	// 	`${process.env.API_URL}/api/member/${memberId}/claims?offset=${pageSize}`,
-	// 	fetcher
-	// );
+const fetcher = (url) => axios.get(url, { headers: { 'x-functions-key': X_FUNCTION_KEY } }).then((res) => res.data);
+
+const getMembersList = (offset = 0, pageSize = 10) => {
 	return useSWR(
-		`https://fa-coreclaims-ios38n2l.azurewebsites.net/api/member?offset=0&limit=${pageSize}`,
+		`${API_URL}/member?offset=${offset}&limit=${pageSize}`,
+		fetcher
+	);
+};
+
+const getMember = (memberId) => {
+	return useSWR(
+		`${API_URL}/member/${memberId}?`,
 		fetcher
 	);
 };
 
 const TransactionsStatement={
     getMembersList,
+    getMember
 }
 
 export default TransactionsStatement;
