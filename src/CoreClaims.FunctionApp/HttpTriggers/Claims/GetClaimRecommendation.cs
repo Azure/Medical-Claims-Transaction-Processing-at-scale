3b71f1fea16fa65b07a808017646ffe15ba91747
Zoiner Tejada <zoinertejada@solliance.net>
2023-06-20 06:28:45 -0400

    Add SK starter

---
+++ src/CoreClaims.FunctionApp/HttpTriggers/Claims/GetClaimRecommendation.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using CoreClaims.FunctionApp.Models.Response;
+using CoreClaims.Infrastructure.Repository;
+using CoreClaims.SemanticKernel;
+
+namespace CoreClaims.FunctionApp.HttpTriggers.Claims
+{
+    public class GetClaimRecommendation
+    {
+        private readonly IClaimRepository _claimRepository;
+        private readonly IRulesEngine _rulesEngine;
+
+        public GetClaimRecommendation(IClaimRepository claimRepository, IRulesEngine rulesEngine)
+        {
+            _claimRepository = claimRepository;
+            _rulesEngine = rulesEngine;
+        }
+
+        [FunctionName("GetClaimRecommendation")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "claim/{claimId}/recommendation")] HttpRequest req,
+            string claimId,
+            ILogger log)
+        {
+            using (log.BeginScope("HttpTrigger: GetClaimRecommendation"))
+            {
+                try
+                {
+                    var claim = await _claimRepository.GetClaim(claimId);
+                    if (claim == null)
+                    {
+                        log.LogError($"Claim {claimId} not found.");
+                        return new NotFoundResult();
+                    }
+
+                    var recommendation = await _rulesEngine.ReviewClaim(claim);
+
+                    log.LogInformation($"Successfully retrieved recommendation for Claim: {claimId}");
+                    return new OkObjectResult(recommendation);
+                }
+                catch (Exception ex)
+                {
+                    return new BadRequestObjectResult(ex.Message);
+                }
+            }
+        }
+    }
+}
