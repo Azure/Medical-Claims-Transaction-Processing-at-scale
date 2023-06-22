87fe4a6f8979a8126fee87c6d316cd1249173218
Carlos Retes <carlos.retes@simpat.tech>
2023-06-22 15:58:22 -0600

    Add Coverage API Request
    Also remove unnecessary options in nav bar: home, search

--- ui/medical-claims-ui/src/app/hooks/TransactionsStatement.js
+++ ui/medical-claims-ui/src/app/hooks/TransactionsStatement.js
@@ -28,7 +28,7 @@ const getMember = (memberId) => {
 
 const getCoverageByMember = (memberId) => {
 	return useSWR(
-		`${API_URL}/member/${memberId}`,
+		`${API_URL}/member/${memberId}/coverage`,
 		fetcher
 	);
 };
@@ -74,7 +74,7 @@ export const updateClaim = (claimId) =>
 export const acknowledgeClaim = (claimId) =>
 	useSWRMutation(`${API_URL}/claim/${claimId}/acknowledge`, post);
 
-const TransactionsStatement={
+const TransactionsStatement = {
     getMembersList,
     getMember,
     getCoverageByMember,
--- ui/medical-claims-ui/src/app/members/memberCoverageModal.js
+++ ui/medical-claims-ui/src/app/members/memberCoverageModal.js
@@ -1,22 +1,26 @@
 import Modal from '../components/modal'
 import TransactionsStatement from '../hooks/TransactionsStatement'
+import Moment from 'moment'
 
 export default function MemberCoverageModal({memberId, showCoverageModal, setShowCoverageModal}){
-	const { data, isLoading } = TransactionsStatement.getCoverageByMember(memberId);
+	const memberRequest = TransactionsStatement.getMember(memberId);
+	const coverageRequest = TransactionsStatement.getCoverageByMember(memberId);
 
 	return(
 		<Modal title='Member Coverage' showModal={showCoverageModal} setShowModal={setShowCoverageModal} showSaveButton={false}>
-			{((!isLoading && data) ? (
+			{((!memberRequest.isLoading && memberRequest.data) ? (
 				<>
 					<h4 className="font-semibold mb-10">
-						{data.firstName} {data.lastName}
+						{memberRequest.data.firstName} {memberRequest.data.lastName}
 					</h4>
-					<div className='grid grid-cols-2'>
-						<div>Start Date:</div>
-						<div>September 9, 2022</div>
-						<div>End Date:</div>
-						<div>September 9, 2023</div>						
-					</div>
+					{!coverageRequest.isLoading ? (
+						<div className='grid grid-cols-2'>
+							<div>Start Date:</div>
+							<div>{ Moment(coverageRequest.data.startDate).format('MMM DD, YYYY') }</div>
+							<div>End Date:</div>
+							<div>{ Moment(coverageRequest.data.endDate).format('MMM DD, YYYY') }</div>						
+						</div>
+					) : null}
 				</>
 			) : null)}
 		</Modal>
