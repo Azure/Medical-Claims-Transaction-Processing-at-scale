9989f0afb9cba243dd6b7ff483e72715315b207b
Matthew Gray <matthew.gray@hatboysoftware.com>
2023-06-24 11:53:01 -0500

    Sorting out errors in Setup-Synapse.ps1

--- deploy/powershell/Setup-Synapse.ps1
+++ deploy/powershell/Setup-Synapse.ps1
@@ -16,21 +16,21 @@ if (-not $workspaceName) {
     $suffix=""
 }
 
-az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsDataLake$suffix --file $(jq '.' "linkedService/CoreClaimsDataLake.json")
-az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsCosmosDb$suffix --file "linkedService/CoreClaimsCosmosDb.json"
+az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsDataLake$suffix --file '@"linkedService/CoreClaimsDataLake.json"'
+az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsCosmosDb$suffix --file '@"linkedService/CoreClaimsCosmosDb.json"'
 
 $datasets = Get-ChildItem ./dataset
 foreach ($dataset in $datasets) {
-    az synapse dataset create --workspace-name $workspaceName --name "${dataset.BaseName}${suffix}" --file "dataset/$dataset"
+    az synapse dataset create --workspace-name $workspaceName --name "${dataset.BaseName}${suffix}" --file "@""./dataset/$dataset"
 }
 
 Pop-Location
 
-az synapse notebook create --workspace-name $workspaceName --file "../Synthea-CSV-to-Json.ipynb" --name Synthea-CSV-to-Json$suffix --folder-path 'Ingestion' --spark-pool-name ingestion
+az synapse notebook create --workspace-name $workspaceName --file '@"../Synthea-CSV-to-Json.ipynb"' --name Synthea-CSV-to-Json$suffix --folder-path 'Ingestion' --spark-pool-name ingestion
 
 Push-Location $(./Join-Path-Recursively.ps1 -pathParts $synapsePath.Split(","))
 
-az synapse pipeline create --workspace-name $workspaceName --file "pipeline/Initial-Ingestion.json" --name Initial-Ingestion$suffix
+az synapse pipeline create --workspace-name $workspaceName --file '@"pipeline/Initial-Ingestion.json"' --name Initial-Ingestion$suffix
 
 Pop-Location
 Pop-Location
