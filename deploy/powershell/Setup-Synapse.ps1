18f55ea6974171f48470aa684b234d4ed0e295f7
Matthew Gray <matthew.gray@hatboysoftware.com>
2023-06-24 11:21:25 -0500

    Adding ability to explicitly set synapse workspace name in case it cannot be created

--- deploy/powershell/Setup-Synapse.ps1
+++ deploy/powershell/Setup-Synapse.ps1
@@ -1,29 +1,36 @@
 #!/usr/bin/pwsh
 
 Param(
-    [parameter(Mandatory=$true)][string]$resourceGroup
+    [parameter(Mandatory=$true)][string]$resourceGroup, 
+    [parameter(Mandatory=$false)][string]$synapseWorkspace,
+    [parameter(Mandatory=$false)][string]$suffix
 )
 
 $synapsePath="..,..,synapse"
 Push-Location $($MyInvocation.InvocationName | Split-Path)
 Push-Location $(./Join-Path-Recursively.ps1 -pathParts $synapsePath.Split(","))
 
-$workspaceName=$(az synapse workspace list -g $resourceGroup -o json | ConvertFrom-Json).name
-az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsDataLake --file ./linkedService/CoreClaimsDataLake.json
-az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsCosmosDb --file ./linkedService/CoreClaimsCosmosDb.json
+$workspaceName = $synapseWorkspace
+if (-not $workspaceName) {
+    $workspaceName=$(az synapse workspace list -g $resourceGroup -o json | ConvertFrom-Json).name
+    $suffix=""
+}
+
+az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsDataLake$suffix --file $(jq '.' "linkedService/CoreClaimsDataLake.json")
+az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsCosmosDb$suffix --file "linkedService/CoreClaimsCosmosDb.json"
 
 $datasets = Get-ChildItem ./dataset
 foreach ($dataset in $datasets) {
-    az synapse dataset create --workspace-name $workspaceName --name $dataset.BaseName --file ./dataset/$dataset
+    az synapse dataset create --workspace-name $workspaceName --name "${dataset.BaseName}${suffix}" --file "dataset/$dataset"
 }
 
 Pop-Location
 
-az synapse notebook create --workspace-name $workspaceName --file "../Synthea-CSV-to-Json.ipynb" --name Synthea-CSV-to-Json --folder-path 'Ingestion' --spark-pool-name ingestion
+az synapse notebook create --workspace-name $workspaceName --file "../Synthea-CSV-to-Json.ipynb" --name Synthea-CSV-to-Json$suffix --folder-path 'Ingestion' --spark-pool-name ingestion
 
 Push-Location $(./Join-Path-Recursively.ps1 -pathParts $synapsePath.Split(","))
 
-az synapse pipeline create --workspace-name $workspaceName --file ./pipeline/Initial-Ingestion.json --name Initial-Ingestion
+az synapse pipeline create --workspace-name $workspaceName --file "pipeline/Initial-Ingestion.json" --name Initial-Ingestion$suffix
 
 Pop-Location
 Pop-Location
