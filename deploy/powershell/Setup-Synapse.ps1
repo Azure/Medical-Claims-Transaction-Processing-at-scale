a20ed5e75ef1e875742ab86e8c4d336fb6f572b0
Matthew Gray <matthew.gray@hatboysoftware.com>
2023-06-24 08:42:05 -0500

    Completing setup script for Synapse

--- deploy/powershell/Setup-Synapse.ps1
+++ deploy/powershell/Setup-Synapse.ps1
@@ -12,5 +12,18 @@ $workspaceName=$(az synapse workspace list -g $resourceGroup -o json | ConvertFr
 az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsDataLake --file ./linkedService/CoreClaimsDataLake.json
 az synapse linked-service create --workspace-name $workspaceName --name CoreClaimsCosmosDb --file ./linkedService/CoreClaimsCosmosDb.json
 
+$datasets = Get-ChildItem ./dataset
+foreach ($dataset in $datasets) {
+    az synapse dataset create --workspace-name $workspaceName --name $dataset.BaseName --file ./dataset/$dataset
+}
+
+Pop-Location
+
+az synapse notebook create --workspace-name $workspaceName --file "../Synthea-CSV-to-Json.ipynb" --name Synthea-CSV-to-Json --folder-path 'Ingestion' --spark-pool-name ingestion
+
+Push-Location $(./Join-Path-Recursively.ps1 -pathParts $synapsePath.Split(","))
+
+az synapse pipeline create --workspace-name $workspaceName --file ./pipeline/Initial-Ingestion.json --name Initial-Ingestion
+
 Pop-Location
 Pop-Location
--- deploy/powershell/Unified-Deploy.ps1
+++ deploy/powershell/Unified-Deploy.ps1
@@ -4,7 +4,7 @@ Param(
     [parameter(Mandatory=$true)][string]$resourceGroup,
     [parameter(Mandatory=$true)][string]$location,
     [parameter(Mandatory=$true)][string]$subscription,
-    [parameter(Mandatory=$true)][string]$suffix="demo",
+    [parameter(Mandatory=$false)][string]$suffix,
     [parameter(Mandatory=$false)][bool]$stepDeployBicep=$true,
     [parameter(Mandatory=$false)][bool]$stepPublishFunctionApp=$true,
     [parameter(Mandatory=$false)][bool]$stepSetupSynapse=$true,
@@ -12,6 +12,16 @@ Param(
 )
 
 Push-Location $($MyInvocation.InvocationName | Split-Path)
+
+if (-not $suffix) {
+    $crypt = New-Object -TypeName System.Security.Cryptography.SHA256Managed
+    $utf8 = New-Object -TypeName System.Text.UTF8Encoding
+    $hash = [System.BitConverter]::ToString($crypt.ComputeHash($utf8.GetBytes($resourceGroup)))
+    $hash = $hash.replace('-','').toLower()
+    $suffix = $hash.Substring(0,5)
+}
+
+Write-Host "Resource suffix is $suffix" -ForegroundColor Yellow
 
 if ($stepLoginAzure) {
     az login
@@ -23,7 +33,7 @@ if ($stepDeployBicep) {
     & ./Deploy-Bicep.ps1 -resourceGroup $resourceGroup -location $location -suffix $suffix
 }
 
-& ./Generate-Config.ps1 -resourceGroup $resourceGroup
+& ./Generate-Config.ps1 -resourceGroup $resourceGroup -suffix $suffix
 
 if ($stepPublishFunctionApp) {
     & ./Publish-FunctionApp.ps1 -resourceGroup $resourceGroup -functionAppPath "..,..,src,CoreClaims.FunctionApp"
