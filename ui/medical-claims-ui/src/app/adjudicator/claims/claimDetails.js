1135f947deb816845818168cf8701edb74cd4162
Carlos Retes <carlos.retes@simpat.tech>
2023-06-26 16:11:22 -0600

    add Maker Recomendation Button on Adjudicators claim list

--- ui/medical-claims-ui/src/app/adjudicator/claims/claimDetails.js
+++ ui/medical-claims-ui/src/app/adjudicator/claims/claimDetails.js
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { useState, useEffect } from 'react'
-import { Table, Spinner, Pagination } from 'flowbite-react';
+import React, { useState, useEffect, useRef } from 'react'
+import { Table, Spinner, Pagination, Modal, Textarea, Button } from 'flowbite-react';
+import { SparklesIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link'
 import Moment from 'moment'
 import TransactionsStatement from '../../hooks/TransactionsStatement'
@@ -13,12 +14,22 @@ let money = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' 
 export default function ClaimDetails({ claimId, requestClaims }){
 	const { data, isLoading, mutate } = TransactionsStatement.GetClaimDetails(claimId);
 
+	const [isRecModalOpen, setIsRecModalOpen] = useState('');
+	const onClickRecommend = () => setIsRecModalOpen(true);
+	const recModalHeader = <div className="text-xl p-4">Recommendation</div>;
+
 	return((!isLoading && data) ? (
 		<>
 			<div className="card">
 				<div className="card-header grid grid-cols-2">
 					<h4 className="card-title">Claim Details</h4>
 					<div className='text-right'><label>Filing Date: </label>{ Moment(data.FilingDate).format('MMMM DD, YYYY') }</div>
+					<div className="justify-end">
+						<Button color="dark" className="p-0" onClick={onClickRecommend}>
+							<SparklesIcon className="h-6 w-6 text-gray-500 mr-3 text-white" />
+							<h4>Make Recommendation</h4>
+						</Button>
+					</div>
 				</div>
 				<div className="card-body">
 					<div className="relative overflow-x-auto sm:rounded">
@@ -46,9 +57,73 @@ export default function ClaimDetails({ claimId, requestClaims }){
 					</div>
 				</div>
 			</div>
+			<FormModal header={recModalHeader} openModal={isRecModalOpen} setOpenModal={setIsRecModalOpen} >
+			  <RecommendActionForm claimId={claimId} setOpenModal={setIsRecModalOpen} openModal={isRecModalOpen} />
+			</FormModal>
 		</>
 	) : <Spinner aria-label="Loading..." />);
 }
+
+const FormModal = ({ children, header, setOpenModal, openModal }) => {
+  return (
+    <Modal show={openModal} size="xl" popup onClose={() => setOpenModal(false)} 
+    	className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+      <Modal.Header className="items-center">{header}</Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+    </Modal>
+  );
+};
+
+const RecommendActionForm = ({ claimId, setOpenModal, openModal }) => {
+	const ref = useRef(null);
+	const [ recommendation, setRecommendation ] = useState('');
+
+	const [form, setForm] = useState({
+	  claimId
+	});
+
+	useEffect(()=>{
+		setRecommendation('');
+	}, [openModal]);
+  
+
+	const {trigger} = TransactionsStatement.GetClaimRecommendation(claimId);
+
+	const [isLoading, setIsLoading] = useState(false);
+	const onClickCancel = () => {
+	  setForm({ claimId: ''});
+	  //ref.current.value = '';
+	  setRecommendation('');
+	  setIsLoading(false);
+	  setOpenModal(false);
+	};
+  
+	const onSubmit = async () => {		
+	  setIsLoading(true);
+	  const response = await trigger({});
+	  console.log(response.trim());
+	  setRecommendation(response.trim());
+	  setIsLoading(false);
+	};
+ 
+	return (
+	  <div className="space-y-6">
+		<div className="mb-4">
+		  <div className="mb-2 block">
+			<Textarea id="results" name="results" value={recommendation} className='h-20'/>
+		  </div>
+		</div>
+		<div className="w-full flex justify-between pt-4">
+		  <Button color="light" onClick={onClickCancel}>
+			Cancel
+		  </Button>
+		  <Button color="dark" onClick={()=>{ onSubmit() }}>
+			{isLoading ? <Spinner color="white" size="md" /> : 'Submit'}
+		  </Button>
+		</div>
+	  </div>
+	);
+  };
 
 function ClaimsActions({claimStatus, claimId, requestClaims }){
 	let status = ClaimStatusMap.CodeToDisplayName(claimStatus);
