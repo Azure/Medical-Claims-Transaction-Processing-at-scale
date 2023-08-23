5d61d254f523f56d1df5d7ac15f06a3682f088fb
joelhulen <joelhulen@gmail.com>
2023-08-22 20:03:30 -0400

    Update logging and app settings

--- src/CoreClaims.WebAPI/Endpoints/Http/BusinessRuleEndpoints.cs
+++ src/CoreClaims.WebAPI/Endpoints/Http/BusinessRuleEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using CoreClaims.Infrastructure.BusinessRules;
+﻿using System.Net;
+using CoreClaims.Infrastructure.BusinessRules;
 using CoreClaims.Infrastructure.Repository;
 using Microsoft.Extensions.Options;
 
@@ -7,15 +8,36 @@ namespace CoreClaims.WebAPI.Endpoints.Http
     public class BusinessRuleEndpoints
     {
         private readonly IOptions<BusinessRuleOptions> _options;
+        private readonly ILogger _logger;
 
-        public BusinessRuleEndpoints(IOptions<BusinessRuleOptions> options)
+        public BusinessRuleEndpoints(IOptions<BusinessRuleOptions> options,
+            ILogger<BusinessRuleEndpoints> logger)
         {
             _options = options;
+            _logger = logger;
         }
 
         public void Map(WebApplication app)
         {
-            // TODO: Add business rule endpoints here.
+            app.MapGet("/business-rules/", async context =>
+                {
+                    var logger = app.ApplicationServices.GetRequiredService<ILogger<BusinessRuleEndpoints>>();
+                    using (logger.BeginScope("HttpTrigger: GetBusinessRules"))
+                    {
+                        if (_options == null)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                            return;
+                        }
+
+                        var response = await context.Response.WriteAsJsonAsync(_options.Value);
+
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            logger.LogError($"Error occurred: {response}");
+                        }
+                    }
+                }).WithName("GetBusinessRules");
         }
     }
 }
