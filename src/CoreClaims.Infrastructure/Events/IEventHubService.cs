27ba6a8a65447056a59429fb4fb25668e84e010f
joelhulen <joelhulen@gmail.com>
2023-08-24 16:15:45 -0400

    Completed EventHub integration into the worker service

--- src/CoreClaims.Infrastructure/Events/IEventHubService.cs
+++ src/CoreClaims.Infrastructure/Events/IEventHubService.cs
@@ -1,7 +1,10 @@
-﻿namespace CoreClaims.Infrastructure.Events
+﻿using Azure.Messaging.EventHubs.Consumer;
+
+namespace CoreClaims.Infrastructure.Events
 {
     public interface IEventHubService
     {
         Task TriggerEventAsync<T>(T eventPayload, string eventHubName);
+        IAsyncEnumerable<PartitionEvent> ReadEvents(string eventHubName, CancellationToken cancellationToken);
     }
 }
