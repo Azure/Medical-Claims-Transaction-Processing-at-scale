28555acb9f86ba9c00e252de292adb3bf25c4587
Sensational Code <sensational.code@gmail.com>
2023-08-25 03:16:37 -0700

    Move loading state into DataTable

--- ui/medical-claims-ui/src/app/providers/DataTable.jsx
+++ ui/medical-claims-ui/src/app/providers/DataTable.jsx
@@ -1,36 +1,42 @@
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Table, Pagination, Spinner } from 'flowbite-react';
 
 
-export default function Datatable({ headers = [], data = [], page = 1, onPageChange }) {
+export default function Datatable({ isLoading, headers = [], data = [], page = 1, onPageChange }) {
 	return (
 		<>
-			{/* Table */}
-			<Table className="w-full" hoverable>
+			{!isLoading ? (
+				/* Table */
+				<Table className="w-full" hoverable>
 
-				{/* Table headers */}
-				<Table.Head>
-					{headers.map((header) => (
-						<Table.HeadCell key={header.key} className="!p-4">
-							{header.name}
-						</Table.HeadCell>
-					))}
-				</Table.Head>
+					{/* Table headers */}
+					<Table.Head>
+						{headers.map((header) => (
+							<Table.HeadCell key={header.key} className="!p-4">
+								{header.name}
+							</Table.HeadCell>
+						))}
+					</Table.Head>
 
-				{/* Table body */}
-				<Table.Body className="divide-y">
-					{data.map((row) => (
-						<Table.Row key={row.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-							{Object.values(headers).map((header, index) => (
-								<Table.Cell key={`${row.id}-${index}`} className="!p-4">
-									{row[header.key] ? row[header.key] : '-'}
-								</Table.Cell>
-							))}
-						</Table.Row>
-					))}
-				</Table.Body>
-			</Table>
+					{/* Table body */}
+					<Table.Body className="divide-y">
+						{data.map((row) => (
+							<Table.Row key={row.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+								{Object.values(headers).map((header, index) => (
+									<Table.Cell key={`${row.id}-${index}`} className="!p-4">
+										{row[header.key] ? row[header.key] : '-'}
+									</Table.Cell>
+								))}
+							</Table.Row>
+						))}
+					</Table.Body>
+				</Table>
+			) :
+				<div className='text-center mt-10 mb-10'>
+					<Spinner aria-label="Loading..." />
+				</div>
+			}
 
 			{/* Pagination */}
 			<Pagination
