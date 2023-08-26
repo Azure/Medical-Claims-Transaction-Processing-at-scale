33a62b7c76655f7758b5b146f0f33ba8ce45832b
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2023-08-26 18:24:31 -0500

    Tweaks to CloudShell-Deploy.ps1 and README

--- deploy/powershell/CloudShell-Deploy.ps1
+++ deploy/powershell/CloudShell-Deploy.ps1
@@ -6,32 +6,27 @@ Param(
     [parameter(Mandatory=$true)][string]$resourceGroup,
     [parameter(Mandatory=$true)][string]$location,
     [parameter(Mandatory=$true)][string]$subscription,
-    [parameter(Mandatory=$false)][string]$openAiName,
-    [parameter(Mandatory=$false)][string]$openAiRg,
-    [parameter(Mandatory=$false)][string]$openAiDeployment,
     [parameter(Mandatory=$false)][string]$suffix,
-    [parameter(Mandatory=$false)][string]$synapseWorkspace,
     [parameter(Mandatory=$false)][bool]$stepDeployBicep=$true,
-    [parameter(Mandatory=$false)][bool]$stepDeployOpenAi=$true,
-    [parameter(Mandatory=$false)][bool]$stepBuildImages=$false,
-    [parameter(Mandatory=$false)][bool]$stepPushImages=$false,
+    [parameter(Mandatory=$false)][bool]$stepBuildPush=$false,
     [parameter(Mandatory=$false)][bool]$stepDeployCertManager=$true,
     [parameter(Mandatory=$false)][bool]$stepDeployTls=$true,
     [parameter(Mandatory=$false)][bool]$stepDeployImages=$true,
     [parameter(Mandatory=$false)][bool]$stepSetupSynapse=$true,
     [parameter(Mandatory=$false)][bool]$stepPublishSite=$true,
-    [parameter(Mandatory=$false)][bool]$stepLoginAzure=$true
+    [parameter(Mandatory=$false)][bool]$stepLoginAzure=$false
 )
-
-# Update the extension to make sure you have the latest version installed
-az extension add --name aks-preview
-az extension update --name aks-preview
 
 az extension add --name  application-insights
 az extension update --name  application-insights
 
 az extension add --name storage-preview
 az extension update --name storage-preview
+
+winget install --id=Kubernetes.kubectl  -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=Microsoft.Azure.Kubelogin  -e --accept-package-agreements --accept-source-agreements --silent
+
+$gValuesFile="configFile.yaml"
 
 Push-Location $($MyInvocation.InvocationName | Split-Path)
 
@@ -51,20 +46,8 @@ if ($stepLoginAzure) {
 
 az account set --subscription $subscription
 
-if ($stepDeployOpenAi) {
-    if (-not $openAiName) {
-        $openAiName="openai-$suffix"
-    }
-
-    if (-not $openAiRg) {
-        $openAiRg=$resourceGroup
-    }
-
-    & ./Deploy-OpenAi.ps1 -name $openAiName -resourceGroup $openAiRg -location $location -suffix $suffix -deployment $openAiDeployment
-}
-
 if ($stepDeployBicep) {
-    & ./Deploy-Bicep.ps1 -resourceGroup $resourceGroup -location $location -suffix $suffix -openAiName $openAiName -openAiRg $openAiRg -openAiDeployment $openAiDeployment
+    & ./Deploy-Bicep.ps1 -resourceGroup $resourceGroup -location $location -suffix $suffix
 }
 
 # Connecting kubectl to AKS
@@ -72,23 +55,24 @@ Write-Host "Retrieving Aks Name" -ForegroundColor Yellow
 $aksName = $(az aks list -g $resourceGroup -o json | ConvertFrom-Json).name
 Write-Host "The name of your AKS: $aksName" -ForegroundColor Yellow
 
+az aks enable-addons -g $resourceGroup -n $aksName --addons http_application_routing
+
 # Write-Host "Retrieving credentials" -ForegroundColor Yellow
-az aks get-credentials -n $aksName -g $resourceGroup --overwrite-existing
+az aks get-credentials -n $aksName -g $resourceGroup --overwrite-existing --admin
 
 # Generate Config
-New-Item -ItemType Directory -Force -Path $(./Join-Path-Recursively.ps1 -pathParts ..,__values)
-$gValuesLocation=$(./Join-Path-Recursively.ps1 -pathParts ..,__values,$gValuesFile)
-& ./Generate-Config.ps1 -resourceGroup $resourceGroup -suffix $suffix -openAiName $openAiName -openAiRg $openAiRg -openAiDeployment $openAiDeployment
+New-Item -ItemType Directory -Force -Path $(./Join-Path-Recursively.ps1 -pathParts ..,..,__values)
+$gValuesLocation=$(./Join-Path-Recursively.ps1 -pathParts ..,..,__values,$gValuesFile)
+& ./Generate-Config.ps1 -resourceGroup $resourceGroup -suffix $suffix -outputFile $gValuesLocation
 
 # Create Secrets
 if ([string]::IsNullOrEmpty($acrName))
 {
     $acrName = $(az acr list --resource-group $resourceGroup -o json | ConvertFrom-Json).name
+    $acrResourceGroup = $resourceGroup
 }
 
 Write-Host "The Name of your ACR: $acrName" -ForegroundColor Yellow
-# & ./Create-Secret.ps1 -resourceGroup $resourceGroup -acrName $acrName
-# az aks update -n $aksName -g $resourceGroup --attach-acr $acrName
 
 if ($stepDeployCertManager) {
     # Deploy Cert Manager
@@ -100,25 +84,16 @@ if ($stepDeployTls) {
     & ./DeployTlsSupport.ps1 -sslSupport prod -resourceGroup $resourceGroup -aksName $aksName
 }
 
-if ($stepBuildImages) {
-    # Build
-    & ./BuildImages.ps1 -resourceGroup $acrResourceGroup -acrName $acrName
-}
-
-if ($stepPushImages) {
-    # Push
-    & ./PushImages.ps1 -resourceGroup $acrResourceGroup -acrName $acrName
+if ($stepBuildPush) {
+    # Build an Push
+    & ./BuildPush.ps1 -resourceGroup $acrResourceGroup -acrName $acrName
 }
 
 if ($stepDeployImages) {
     # Deploy images in AKS
-    $gValuesLocation=$(./Join-Path-Recursively.ps1 -pathParts ..,__values,$gValuesFile)
+    $gValuesLocation=$(./Join-Path-Recursively.ps1 -pathParts ..,..,__values,$gValuesFile)
     $chartsToDeploy = "*"
-    & ./Deploy-Images-Aks.ps1 -aksName $aksName -resourceGroup $resourceGroup -charts $chartsToDeploy -acrName $acrName -acrResourceGroup $acrResourceGroup -valuesFile $gValuesLocation
-}
-
-if ($stepPublishFunctionApp) {
-    & ./Publish-FunctionApp.ps1 -resourceGroup $resourceGroup -projectName "CoreClaims.FunctionApp"
+    & ./Deploy-Images-Aks.ps1 -aksName $aksName -resourceGroup $resourceGroup -charts $chartsToDeploy -acrName $acrName -valuesFile $gValuesLocation
 }
 
 if ($stepSetupSynapse) {
