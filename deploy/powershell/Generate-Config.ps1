2838259ad04f3ae3c77a31adf8f28d8c856fab8f
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2023-08-26 12:39:50 -0500

    Initial pass at adding AKS to claims demo.

--- deploy/powershell/Generate-Config.ps1
+++ deploy/powershell/Generate-Config.ps1
@@ -1,9 +1,11 @@
 Param(
     [parameter(Mandatory=$true)][string]$resourceGroup,
-    [parameter(Mandatory=$false)][string]$openAiName,
-    [parameter(Mandatory=$false)][string]$openAiRg,
-    [parameter(Mandatory=$false)][string]$openAiDeployment,
-    [parameter(Mandatory=$true)][string]$suffix
+    [parameter(Mandatory=$true)][string]$suffix,
+    [parameter(Mandatory=$false)][string[]]$outputFile=$null,
+    [parameter(Mandatory=$false)][string[]]$gvaluesTemplate="..,..,gvalues.template.yml",
+    [parameter(Mandatory=$false)][string[]]$dockerComposeTemplate="..,..,docker-compose.template.yml",
+    [parameter(Mandatory=$false)][string]$ingressClass="addon-http-application-routing",
+    [parameter(Mandatory=$false)][string]$domain
 )
 
 function EnsureAndReturnFirstItem($arr, $restype) {
@@ -27,7 +29,7 @@ if (-not $rg) {
 $tokens=@{}
 
 ## Getting Datalake info
-$dataLakeEndpoint=$(az storage account list -g $resourceGroup -o json | ConvertFrom-Json)[0].primaryEndpoints.dfs
+$dataLakeAccountName=$(az storage account list -g $resourceGroup -o json | ConvertFrom-Json)[0].name
 
 ## Getting Function App info
 $functionAppHostname=$(az functionapp list -g $resourceGroup -o json | ConvertFrom-Json).hostNames
@@ -35,7 +37,6 @@ $functionAppHostname=$(az functionapp list -g $resourceGroup -o json | ConvertFr
 ## Getting CosmosDb info
 $docdb=$(az cosmosdb list -g $resourceGroup --query "[?kind=='GlobalDocumentDB'].{name: name, kind:kind, documentEndpoint:documentEndpoint}" -o json | ConvertFrom-Json)
 $docdb=EnsureAndReturnFirstItem $docdb "CosmosDB (Document Db)"
-$docdbKey=$(az cosmosdb keys list -g $resourceGroup -n $docdb.name -o json --query primaryMasterKey | ConvertFrom-Json)
 Write-Host "Document Db Account: $($docdb.name)" -ForegroundColor Yellow
 
 ## Getting EventHub info
@@ -55,35 +56,57 @@ if ($appInsightsName -and $appInsightsName.Length -eq 1) {
 Write-Host "App Insights Instrumentation Key: $appinsightsId" -ForegroundColor Yellow
 
 ## Getting OpenAI info
-if ($openAiName) {
-    $openAi=$(az cognitiveservices account show -n $openAiName -g $openAiRg -o json | ConvertFrom-Json)
-} else {
-    $openAi=$(az cognitiveservices account list -g $openAiRg --query "[?kind=='OpenAI'].{name: name, kind:kind, endpoint: properties.endpoint}" -o json | ConvertFrom-Json)
-}
+$openAi=$(az cognitiveservices account list -g $resourceGroup --query "[?kind=='OpenAI'].{name: name, kind:kind, endpoint: properties.endpoint}" -o json | ConvertFrom-Json)
 
-$openAiKey=$(az cognitiveservices account keys list -g $openAiRg -n $openAi.name -o json --query key1 | ConvertFrom-Json)
+$openAiKey=$(az cognitiveservices account keys list -g $resourceGroup -n $openAi.name -o json --query key1 | ConvertFrom-Json)
 
-if (-not $openAiDeployment) {
-    $openAiDeployment = "completions"
-}
+$openAiDeployment = "completions"
 
+$apiIdentityClientId=$(az identity show -g $resourceGroup -n mi-api-coreclaims-$suffix -o json | ConvertFrom-Json).clientId
+$workerIdentityClientId=$(az identity show -g $resourceGroup -n mi-worker-coreclaims-$suffix -o json | ConvertFrom-Json).clientId
+$tenantId=$(az account show --query homeTenantId --output tsv)
 ## Showing Values that will be used
 
 Write-Host "===========================================================" -ForegroundColor Yellow
 Write-Host "settings.json files will be generated with values:"
 
 $tokens.suffix=$suffix
-$tokens.cosmosKey=$docdbKey
 $tokens.cosmosEndpoint=$docdb.documentEndpoint
-$tokens.dataLakeEndpoint=$datalakeEndpoint
+$tokens.dataLakeAccountName=$dataLakeAccountName
 $tokens.eventHubKey=$eventHubKey
-$tokens.openAiEndpoint=$openAi.properties.endpoint
+$tokens.openAiEndpoint=$openAi.endpoint
 $tokens.openAiKey=$openAiKey
-$tokens.openAiDeployment=$openAiDeployment
-$tokens.apiUrl="https://${functionAppHostName}/api"
+$tokens.openAiCompletionsDeployment=$openAiDeployment
+$tokens.apiClientId=$apiIdentityClientId
+$tokens.workerClientId=$workerIdentityClientId
+$tokens.tenantId=$tenantId
+
+# Standard fixed tokens
+$tokens.ingressclass=$ingressClass
+$tokens.ingressrewritepath="(/|$)(.*)"
+$tokens.ingressrewritetarget="`$2"
+
+if($ingressClass -eq "nginx") {
+    $tokens.ingressrewritepath="(/|$)(.*)" 
+    $tokens.ingressrewritetarget="`$2"
+}
 
 Write-Host ($tokens | ConvertTo-Json) -ForegroundColor Yellow
 Write-Host "===========================================================" -ForegroundColor Yellow
+
+Push-Location $($MyInvocation.InvocationName | Split-Path)
+$gvaluesTemplatePath=$(./Join-Path-Recursively -pathParts $gvaluesTemplate.Split(","))
+Write-Host $gvaluesTemplatePath
+$outputFilePath=$(./Join-Path-Recursively -pathParts $outputFile.Split(","))
+Write-Host $outputFilePath
+& ./Token-Replace.ps1 -inputFile $gvaluesTemplatePath -outputFile $outputFilePath -tokens $tokens
+Pop-Location
+
+Push-Location $($MyInvocation.InvocationName | Split-Path)
+$dockerComposeTemplatePath=$(./Join-Path-Recursively -pathParts $dockerComposeTemplate.Split(","))
+$outputFilePath=$(./Join-Path-Recursively -pathParts ..,..,docker-compose.yml)
+& ./Token-Replace.ps1 -inputFile $dockerComposeTemplatePath -outputFile $outputFilePath -tokens $tokens
+Pop-Location
 
 $publisherSettingsTemplate="..,..,src,CoreClaims.Publisher,settings.template.json"
 $publisherSettings="..,..,src,CoreClaims.Publisher,settings.json"
