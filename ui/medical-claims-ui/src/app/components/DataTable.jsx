bdc56b4696b44f504967cc176244446654e1d972
Sensational Code <sensational.code@gmail.com>
2023-08-27 14:47:21 -0700

    DataTable do not render if values are undefined

--- ui/medical-claims-ui/src/app/components/DataTable.jsx
+++ ui/medical-claims-ui/src/app/components/DataTable.jsx
@@ -46,9 +46,9 @@ export default function DataTable(props) {
 
 					{/* Table body */}
 					<Table.Body className="divide-y">
-						{data.map((row, rowIndex) => (
+						{data && data.map((row, rowIndex) => (
 							<Table.Row key={rowIndex} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-								{Object.values(headers).map((header, cellIndex) => (
+								{headers && Object.values(headers).map((header, cellIndex) => (
 									<Table.Cell key={`${rowIndex}-${cellIndex}`} style={header.itemStyle}>
 										{formatRowItem(header, row[header.key], row)}
 									</Table.Cell>
