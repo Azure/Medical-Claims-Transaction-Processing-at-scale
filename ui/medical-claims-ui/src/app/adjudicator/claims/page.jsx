b200f4f315340546ba5f02aa898d1a7c2a4a7918
Sensational Code <sensational.code@gmail.com>
2023-08-27 14:46:42 -0700

    Formatting

--- ui/medical-claims-ui/src/app/adjudicator/claims/page.jsx
+++ ui/medical-claims-ui/src/app/adjudicator/claims/page.jsx
@@ -8,15 +8,15 @@ import './style.css';
 export default function Adjudicator() {
 	return(
 		<>
-			<h3 className='text-3xl'>
+			<h3 className="text-3xl">
 				Claims to Assigned to Adjudicator
 			</h3>
-			<Tabs.Group id="claimsAdjudicatorTabs" aria-label="Default tabs" style="default" className='mt-5'>
+			<Tabs.Group id="claimsAdjudicatorTabs" aria-label="Default tabs" style="default" className="mt-5">
 				<Tabs.Item active title="Non-Manager">
-					<ClaimsByAdjudicator adjudicatorId='df166300-5a78-3502-a46a-832842197811' isManager={false}/>
+					<ClaimsByAdjudicator adjudicatorId="df166300-5a78-3502-a46a-832842197811" isManager={false}/>
 				</Tabs.Item>
 					<Tabs.Item title="Manager">
-					<ClaimsByAdjudicator adjudicatorId='a735bf55-83e9-331a-899d-a82a60b9f60c' isManager={true}/>
+					<ClaimsByAdjudicator adjudicatorId="a735bf55-83e9-331a-899d-a82a60b9f60c" isManager={true}/>
 				</Tabs.Item>
 			</Tabs.Group>
 		</>
