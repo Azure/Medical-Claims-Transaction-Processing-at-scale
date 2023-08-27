10bebc0af873c0ca29979db5908a619ae1369adb
Sensational Code <sensational.code@gmail.com>
2023-08-27 03:23:25 -0700

    Initial home page text

--- ui/medical-claims-ui/src/app/page.jsx
+++ ui/medical-claims-ui/src/app/page.jsx
@@ -2,7 +2,16 @@
 export default function Home() {
 	return(
 		<>
-			Home
+			<h1>Welcome to <b>Medical Claims Handling</b></h1>
+			<p>This site is the interface for a medical claims management solution.
+				Members have insurance coverage and submit claims to cover various procedures.
+				Providers deliver services to the members and payers provide the insurance coverage 
+				that pays providers for their services.</p>
+			<p>Claims submitted are submitted in a stream and loaded into the backing database for review and approval.</p>
+			<p>Business rules govern the automated or human approval of claims.</p>
+			<p>An AI-powered co-pilot empowers agents with recommendations on how to process the claim.</p>
+
+			<div></div>
 		</>
 	);
 }
