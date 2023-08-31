5f803b641f8f6f6c848946cf902bc9242f0476fa
Sensational Code <sensational.code@gmail.com>
2023-08-31 08:59:44 -0700

    Hide open cards on members page on table page change

--- ui/medical-claims-ui/src/app/member/claims/ClaimList.jsx
+++ ui/medical-claims-ui/src/app/member/claims/ClaimList.jsx
@@ -50,13 +50,13 @@ export default function ClaimList({ memberId }) {
 	const [ showClaimDetail, setShowClaimDetail ] = useState(false);
 	const [ showHistory, setShowHistory ] = useState(false);
 
-	const viewDetails = (newClaimId)=> {
+	const viewDetails = (newClaimId) => {
 		setClaimId(newClaimId);
 		setShowClaimDetail(true);
 		setShowHistory(false);
 	}
 
-	const viewHistory = (newClaimId)=> {
+	const viewHistory = (newClaimId) => {
 		setClaimId(newClaimId);
 		setShowHistory(true);
 		setShowClaimDetail(false);
