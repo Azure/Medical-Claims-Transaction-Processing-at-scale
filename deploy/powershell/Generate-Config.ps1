db20437c1299fccf678f903503922accd635c62d
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2023-09-01 10:02:04 +0300

    Prepare for Cloud Shell deployment

--- deploy/powershell/Generate-Config.ps1
+++ deploy/powershell/Generate-Config.ps1
@@ -3,7 +3,6 @@ Param(
     [parameter(Mandatory=$true)][string]$suffix,
     [parameter(Mandatory=$false)][string[]]$outputFile=$null,
     [parameter(Mandatory=$false)][string[]]$gvaluesTemplate="..,..,gvalues.template.yml",
-    [parameter(Mandatory=$false)][string[]]$dockerComposeTemplate="..,..,docker-compose.template.yml",
     [parameter(Mandatory=$false)][string]$ingressClass="addon-http-application-routing",
     [parameter(Mandatory=$false)][string]$domain
 )
@@ -107,12 +106,6 @@ Write-Host $gvaluesTemplatePath
 $outputFilePath=$(./Join-Path-Recursively -pathParts $outputFile.Split(","))
 Write-Host $outputFilePath
 & ./Token-Replace.ps1 -inputFile $gvaluesTemplatePath -outputFile $outputFilePath -tokens $tokens
-Pop-Location
-
-Push-Location $($MyInvocation.InvocationName | Split-Path)
-$dockerComposeTemplatePath=$(./Join-Path-Recursively -pathParts $dockerComposeTemplate.Split(","))
-$outputFilePath=$(./Join-Path-Recursively -pathParts ..,..,docker-compose.yml)
-& ./Token-Replace.ps1 -inputFile $dockerComposeTemplatePath -outputFile $outputFilePath -tokens $tokens
 Pop-Location
 
 $publisherSettingsTemplate="..,..,src,CoreClaims.Publisher,settings.template.json"
